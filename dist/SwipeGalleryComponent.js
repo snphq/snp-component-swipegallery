be255d5d30a68936506b1be31025afbda5fef88e
Maksim Koretskiy <maksim.koretskiy@saltpepper.ru>
2015-04-20 20:45:58 +0300

    Build js =<

--- dist/SwipeGalleryComponent.js
+++ dist/SwipeGalleryComponent.js
@@ -1,4 +1,4 @@
-/*! snp-component-swipegallery 0.1.1 */
+/*! snp-component-swipegallery 0.3.0 */
 var extend = function(child, parent) { for (var key in parent) { if (hasProp.call(parent, key)) child[key] = parent[key]; } function ctor() { this.constructor = child; } ctor.prototype = parent.prototype; child.prototype = new ctor(); child.__super__ = parent.prototype; return child; },
   hasProp = {}.hasOwnProperty;
 
